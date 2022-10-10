4ec464fac839a8aa78483b70ba8bbc2744e4c165
Jayaram Kuchibhotla <jayaram1125@gmail.com>
2022-10-10 01:17:50 -0700

    Add files via upload

--- VAEDataAug.py
+++ VAEDataAug.py
@@ -2,7 +2,7 @@ import gzip
 import pickle
 import torch
 import torch.nn as nn
-
+import torch.distributions
 
 
 def encoder_net(input_dim = 784 ,hidden_dim=200,latent_dim = 100):
@@ -11,7 +11,7 @@ def encoder_net(input_dim = 784 ,hidden_dim=200,latent_dim = 100):
 		nn.ReLU(inplace=True),
 		nn.Linear(hidden_dim,hidden_dim),
 		nn.ReLU(inplace=True),
-		nn.Linear(hidden_dim,latent_dim)
+		nn.Linear(hidden_dim,latent_dim*2)
 	)
 	return model
 
@@ -27,46 +27,32 @@ def decoder_net(latent_dim = 100,hidden_dim=200,output_dim = 784):
 
 
 class Encoder(nn.Module):
-	def __init__ (self):
-		pass
+	def __init__ (self,latent_dim):
+		super().__init__()
+		self.network = encoder_net()
+		self.latent_dim = latent_dim 
+		self.softplus = torch.nn.Softplus()
 
 	def forward(self,x):
-		self.encoder_net_logits = encoder_net(x) 
-		mu = self.encoder_net_logits 
-		sigma = torch.nn.Softplus(self.encoder_net_logits)
+		encoder_net_logits = self.network(x) 		
+		mu = encoder_net_logits[:,:self.latent_dim]
+		sigma = self.softplus(encoder_net_logits[:,self.latent_dim:])
 		return mu,sigma
-
 
 class Decoder(nn.Module):
 	def __init__(self):
-		pass
-
-	def forward(self):
-		self.decoder_net_logits = decoder_net()
-		return self.decoder_net_logits
-
-class NormalLogProb(nn.Module):
-	def __init__(self):
 		super().__init__()
+		self.network = decoder_net()
 
-	def forward(self, loc, scale, z):
-		var = torch.pow(scale, 2)
-		return -0.5 * torch.log(2 * np.pi * var) - torch.pow(z - loc, 2) / (2 * var)
-
-class BernoulliLogProb(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.bce_with_logits = nn.BCEWithLogitsLoss(reduction="none")
-
-	def forward(self, logits,target):
-		# bernoulli log prob is equivalent to negative binary cross entropy
-		return self.bce_with_logits(logits,target)
-
+	def forward(self,x):
+		decoder_net_logits = self.network(x) 
+		return decoder_net_logits
 
 class VAEDataAug:
-
 	def __init__(self):
-		pass
+		self.latent_dim = 100
+		self.encoder_model = Encoder(self.latent_dim)
+		self.decoder_model = Decoder()
 
 	def load_and_split_mnist_data(self):
 		filename = 'mnist.pkl.gz'
@@ -83,74 +69,69 @@ class VAEDataAug:
 		self.validation_input, self.validation_label = validation_data
 		self.test_input, self.test_label = test_data
 
+
+	def calculate_kl_divergence(self,mu,sigma):
+		# 0.5*[−∑(log𝜎^2) -n +∑𝜎^2+∑𝜇^2]
+		# 𝜇 is mean and 𝜎 is the standard deviation of distribution q_z  
+		return 0.5*(-torch.sum(torch.log(torch.pow(sigma,2))) -self.latent_dim + torch.sum(torch.pow(sigma,2)) + torch.sum(torch.pow(mu,2)))
+
 	def execute(self,print_frequency=1000,set_grad_val = True):
 		with torch.set_grad_enabled(set_grad_val):
-			
-			latent_dim = 100 
-			
-			encoder_model = Encoder()
-			decoder_model = Decoder()
-			
-			bernoulli_log_prob = BernoulliLogProb() 
-			normal_log_prob = NormalLogProb()
 
-			p_z = normal_log_prob(torch.zeros(latent_dim,dtype=torch.float32),sigma,torch.ones(latent_dim,dtype=torch.float32))
-	
-			
+			p_z = torch.distributions.normal.Normal(torch.zeros(self.latent_dim,dtype=torch.float32),torch.ones(self.latent_dim,dtype=torch.float32))
+
 			if set_grad_val:
 				data = self.train_input
+				optimizer = torch.optim.Adam(list(self.encoder_model.parameters()) + list(self.decoder_model.parameters()),lr=0.001)
 			else:
 				data = self.test_input	
-				optimizer = torch.optim.Adam(list(encoder_model.parameters()) + list(decoder_model.parameters()),lr=0.001)
-
 
 			for i in range(0,len(data)):
+				mu,sigma = self.encoder_model(torch.from_numpy(data[i]).type(torch.float32).reshape(1,784))
+
+				q_z = torch.distributions.normal.Normal(mu,sigma)
+
+				# KL divergence between q_z and latent prior p_z,where p_z is multivariate gaussian
+				#distribution with mean =0 and standard deviation =1
+
+				kl_divergence = self.calculate_kl_divergence(mu,sigma) 
+				decoder_net_logits = self.decoder_model(q_z.sample())
+
+				p_x_given_z = torch.distributions.bernoulli.Bernoulli(logits = decoder_net_logits)
 				
-				mu,sigma = encoder_model(data[i])
-				q_z = normal_log_prob(mu,sigma)
 
-
-				decoder_net_logits = decoder_model(q_z.sample())
-
-				p_x_given_z = bce_logits(self.decoder_net_logits)
+				data_tensor = torch.from_numpy(data[i]).type(torch.float32).reshape(1,784)
+				data_tensor_out = (data_tensor>0.0).float()
 				
-				predicted_samples = p_x_given_z.sample()
+				expected_loglikelihood = torch.sum(p_x_given_z.log_prob(data_tensor_out)) 
 
-				kl_divergence =  torch.mean(q_z * (torch.log(q_z) - p_z))
-
-				elbo = loglikelihood-kl_divergence
+				elbo = expected_loglikelihood-kl_divergence
 
 				if set_grad_val:
 					loss = -elbo 
 					optimizer.zero_grad()
 					loss.backward()
+					optimizer.step()
 
+				if i%print_frequency ==0 or i == len(data)-1:
+					print("Iteration:%d,Elbo=%f,Expected Loglikelihood=%f"%(i,elbo,expected_loglikelihood))
 
-			if i%print_frequency ==0:
-				print("\nelbo=%f,loglikelihood=%f"%(elbo,loglikelihood))
-				print(elbo)
-
-
-	def train(self)
+	def train(self):
+		print("\n Train")
 		self.execute(print_frequency=5000,set_grad_val =True)
 
 
 	def test(self):	
+		print("\n Test")
 		self.execute(print_frequency=1000,set_grad_val =False)
 
 
 if __name__ == "__main__":
 	obj = VAEDataAug()
 	obj.load_and_split_mnist_data()
-
-	print(len(obj.train_input))
-	print(len(obj.validation_input))
-	print(len(obj.test_input))
-
-	print(len(obj.train_label))
-	print(len(obj.validation_label))
-	print(len(obj.test_label))
-
+	
+	print("Train data set size = %d"%(len(obj.train_input)))
+	print("Test data set size = %d"%(len(obj.test_input)))
 
 	obj.train()
 	obj.test()
